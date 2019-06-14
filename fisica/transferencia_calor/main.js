2dd5fdcb2fe002d66a20da4493b958f0e5377028
Enrique Mireles <enrique.mireles@udem.edu>
2019-06-14 14:09:57 -0500

    Label box y edición del canvas.

--- fisica/transferencia_calor/main.js
+++ fisica/transferencia_calor/main.js
@@ -1,3 +1,100 @@
+
+
+// Lines class
+var Lines = function() {
+
+  this.step = 0.5;
+
+  this.coords = [];
+  this.vel = -1;
+  this.xMax = 2;
+  this.xMin = -2;
+  this.y = 0;
+  this.color = p$.BOX_COLORS.YELLOW.BORDER;
+
+  this.arrowWidth = 0;
+  this.arrowHeight = 0;
+  this.arrowSide = 0;
+
+
+}
+
+Lines.prototype.reset = function(xMin, xMax, y, diameter) {
+
+  this.xMax = xMax;
+  this.xMin = xMin;
+  this.y = y;
+
+  this.vel = 0;
+  
+  var length = Math.abs(xMax) + Math.abs(xMin);
+  var amountOfArrows = Math.floor(length) || 1;
+
+  var scale = diameter > 1.5 ? 1.5 : diameter;
+  this.arrowWidth = 0.38 * scale;
+  this.arrowHeight = 0.2 * scale;
+  this.arrowSide = 0.4 * scale;
+
+  this.coords = [];
+  for (var i = 0; i < amountOfArrows; i++) {
+    this.coords.push([this.xMin + i * length / Math.floor(length), this.y]);
+  }
+
+
+
+}
+
+Lines.prototype.draw = function(s) {
+
+  if (this.vel === 0) return;
+
+  s.stroke(this.color);
+  s.fill(this.color);
+  
+  for (var i = 0; i < this.coords.length; i++) {
+
+    // Calcular la nueva posicion.
+    this.coords[i][0] += this.vel * 1.0 / 60.0;
+
+    // Si la velocidad es positiva, detectar cuando pasen por el lado derecho.
+    if (this.vel > 0) {
+
+      // Si ya se paso del lado derecho mover particula al inicio.
+      if (this.coords[i][0] > this.xMax) {
+        this.coords[i][0] = this.xMin;
+      }
+
+    } else {
+      // Detectar cuando pasen por el lado izquierdo.
+
+      if (this.coords[i][0] < this.xMin) {
+        // Reiniciar la posición de la particula.
+        this.coords[i][0] = this.xMax;
+      }
+
+    }
+    
+    this.coords[i][1] = this.y;
+
+    s.save();
+    s.noStroke();
+    s.translate(this.coords[i][0], this.coords[i][1]);
+    if (this.vel < 0) {
+      s.rotate(180);
+    }
+    s.fill(this.color);
+    s.rect(-this.arrowWidth/2 - this.arrowSide * p$.SIN60 / 2, -this.arrowHeight/2, this.arrowWidth, this.arrowHeight);
+    s.equilateralTriangle(0, 0, this.arrowSide, 0);
+    s.restore();
+
+    
+
+  }
+}
+
+var lines1 = new Lines();
+var lines2 = new Lines();
+
 // Constants
 var BOX_L = 2.5;
 var BOX_HL = BOX_L / 2;
@@ -47,6 +144,9 @@ function setup() {
   
   // Configure the world.
   w = new p$.World("canvasContainer", draw, resize);
+  w.axis.isDraggable = false;
+  w.axis.display = false;
+  w.color = p$.BOX_COLORS.GRAY.BACKGROUND;
 
   // Configure the z index of all objects.
 
@@ -87,9 +187,17 @@ function setupControls() {
 
 // Set the initial state of all variables.
 function reset() {
+
   Bar_width = controls.Bar_width.value; //Gets bar width from slider
+  Bar_height = controls.Bar_height.value; //Gets bar height from slider
+  Bw_half = controls.Bar_width.value / 2;
   Bh_half = controls.Bar_height.value / 2;
 
+  // Definir los extremos de las flechas.
+  lines1.reset(-Bw_half, Bw_half, Y_OFFSET / 2, controls.Bar_height.value);
+  lines2.reset(-Bw_half, Bw_half*2, 4, controls.Bar_height.value);
+  lines2.vel = 1;
+  
   t = 0;
   t_final = 3;
   started = false;
@@ -102,13 +210,12 @@ function reset() {
   
   //H un solo conductor
   H_1 = k_cu * (Math.PI * Bh_half *Bh_half) * (t2-t1) /  Bar_width 
+
 }
 
 function drawSimulation() {
-  
-  Bar_height = controls.Bar_height.value; //Gets bar height from slider
-  Bw_half = controls.Bar_width.value / 2;
-  Bh_half = controls.Bar_height.value / 2;
+
+
   x_leftcube = -1*Bw_half-BOX_HL-0.3; //x-coordinate of the bottom left side of the cube
   
 
@@ -167,9 +274,8 @@ function drawSimulation() {
     sim.end();
 
     sim.restore();
-
+    
   }
-
   
   drawCube(x_leftcube, 0, BOX_L, tempColor(t1)); //Function to draw the first cube
   
@@ -177,7 +283,7 @@ function drawSimulation() {
   sim.strokeWeight(2);
   sim.stroke(p$.BOX_COLORS.YELLOW.BORDER);
   sim.fill(p$.BOX_COLORS.YELLOW.BACKGROUND);
-
+  
   sim.save();
   sim.translate(-1*Bw_half, Y_OFFSET/2);
   sim.ellipse(0, 0, 0.2, Bh_half, 90, 270); //Draws the half ellipse of the stick
@@ -186,8 +292,13 @@ function drawSimulation() {
   sim.noStroke();
   sim.rect(0, -1 * Bh_half,Bar_width, Bar_height); //Draws the stick
   sim.restore();
+  
+  lines1.draw(sim);
+  lines2.draw(sim);
 
   drawCube(Bw_half+BOX_HL, 0, BOX_L, tempColor(t2)); //Function to draw the second cube
+  
+
 }
 
 function drawBaseArrow() {
@@ -211,8 +322,15 @@ function draw() {
     if (Math.abs(t2 - t1) > 1) {
 
       t_diff = t2 - t1;
-      t2 -= t_diff * 0.01;
-      t1 += t_diff * 0.01;
+      t2 -= t_diff * 0.005;
+      t1 += t_diff * 0.005;
+
+      lines1.vel = -t_diff / 50;
+      if (lines1.vel > 2) {
+        lines1.vel = 2;
+      } else if (lines1.vel < -2) {
+        lines1.vel = -2;
+      }
   
       console.log(t1, t2);
       t += 1 / 60.0;
@@ -226,7 +344,6 @@ function draw() {
     
   }
 
-
 }
 
 /**
@@ -235,8 +352,12 @@ function draw() {
 function resize() {
   w.axis.setPosition(w.width / 2, w.height / 2);
 
-  /* if (w.width<450){
-    w.scaleX.set(50,5,"");
-  } */
+  if (w.width < 450){
+    w.scaleX.set(50, 1.5, "");
+    w.scaleY.set(50, -1.5, "");
+  } else {
+    w.scaleX.set(50, 1, "");
+    w.scaleY.set(50, -1, "");
+  }
 
 }
